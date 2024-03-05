f877b6375f1982d5d365e58c18d3ce32832b8248
Игорь Воинов <igorvoinov@MacBook-Air-Igor.local>
2024-03-05 03:44:33 +0300

    best version

--- 1/hw1.py
+++ 1/hw1.py
@@ -1,4 +1,5 @@
 from memory_profiler import profile
+import sys
 # p, v = list(map(int, input().split()))
 # q, m = list(map(int, input().split()))
 
@@ -9,37 +10,81 @@ from memory_profiler import profile
 # tree2.clear()
 # print(len(res))
 
+""" 
+-100000000 100000000
+100000000 100000000 (2)
 
-@profile
-def split_line(string, start=0):
-    num = ''
-    for i in range(start, len(string)):
-        if string[i].isdigit():
-            num += string[i]
-        else:
-            break
-    return int(num)
+8 8
+15 5 (3)
 
+0 7
+12 5 (2)
 
-@profile
-def result(str1, str2):
-    # p, v = list(map(int, input().split()))
-    # q, m = list(map(int, input().split()))
-    # user_v = input()
-    # user_m = input()
+-1 12
+8 17  (2)
 
-    p = split_line(user_v)
-    v = split_line(user_v, user_v.find(' ') + 1)
+2 3
+10 3 (1)
 
-    q = split_line(user_m)
-    m = split_line(user_m, user_m.find(' ') + 1)
-    # print(p, v, q, m)
-
-    tree1 = set(range(p-v, p+v+1))
-    tree2 = set(range(q-m, q+m+1))
-    res = tree1.union(tree2)
-    print(len(res))
+15 5
+8 8
+"""
 
 
-if __name__ == '__main__':
-    result()
+from itertools import chain
+p, v = map(int, input().split())
+q, m = map(int, input().split())
+
+left_v = p-v
+right_v = p+v
+left_m = q-m
+right_m = q+m
+
+if left_v > right_m or left_m > right_v:  # 2 3 10 3 -> -1 5 7 13
+    # если дубликаты присутствуют
+    print(sum(1 for _ in chain(range(left_v, right_v+1), range(left_m, right_m+1))))
+    # for i in chain(range(left_v, right_v+1), range(left_m, right_m+1)):
+    #     print(i)
+    print(111)
+elif left_v == right_m or left_m == right_v:  # -100000000 100000000 100000000 100000000 & 0 7 12 5
+    print(sum(1 for _ in chain(range(left_v, right_v), range(left_m, right_m+1))))
+    # for i in chain(range(left_v, right_v), range(left_m, right_m+1)):
+    #     print(i)
+    print(222)
+elif right_m > right_v or left_v > right_v:  # 8 8 15 5 -> 0 16 10 20
+    print(sum(1 for _ in chain(range(left_v, left_m), range(left_m, right_m+1))))
+    # for i in chain(range(left_v, left_m), range(left_m, right_m+1)):
+    #     print(i)
+    print('+++')
+elif left_v < left_m and right_v > right_m:  # 0 2 0 1 -> -2 2
+    # если range без дубликатов
+    print(sum(1 for _ in chain(range(left_v, right_v+1))))
+    # for i in  chain(range(left_v, right_v+1), range(q-m, q+m+1)):
+    #     print(i)
+    print(333)
+elif left_v > left_m and right_v < right_m:  # 15 5 8 8 -> 0 16 10 20
+    # если range без дубликатов
+    print(sum(1 for _ in chain(range(left_m, right_m+1))))
+    # for i in  chain(range(left_v, right_v+1), range(q-m, q+m+1)):
+    #     print(i)
+    print(444)
+else:
+    print('error')
+
+print('final')
+
+p, v = map(int, input().split())
+q, m = map(int, input().split())
+
+tree1 = set(range(p-v, p+v+1))
+tree2 = set(range(q-m, q+m+1))
+res = tree1.union(tree2)
+tree1.clear()
+tree2.clear()
+for i in res:
+    print(i)
+print(len(res))
+
+
+""" сделать переменную счетчик, которая хранит последний номер из генератора и 
+отсортировать генераторы по алфавиту """
